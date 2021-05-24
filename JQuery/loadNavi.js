eecac2d092f3a2fc906eb1928eaf0689e1d698ab
Lumek4 <lumek4@gmail.com>
2021-05-24 18:08:51 +0200

    maybe fix links try?

--- JQuery/loadNavi.js
+++ JQuery/loadNavi.js
@@ -1,4 +1,14 @@
 
 	$().ready(function() {
-		$('#navi').load( 'nav.html');
+		var location = window.location.origin;
+		var depth = window.location.pathname.split('/').length;
+		if(!location.includes('0.0')){
+			depth--;
+		}
+		var path = "";
+		for(var i = 0; i<depth; i++){
+			path += '../';
+		}
+		path += 'nav.html';
+		$('#navi').load(path);
 	});
