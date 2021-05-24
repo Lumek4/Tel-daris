abaa4efcff1cd9d93235022f5eb62005b9610baa
Lumek4 <lumek4@gmail.com>
2021-05-24 19:41:12 +0200

    nav fix

--- JQuery/loadNavi.js
+++ JQuery/loadNavi.js
@@ -1,11 +1,12 @@
 
 	$().ready(function() {
 		var location = window.location.origin;
-		var depth = window.location.pathname.split('/').length-1;
-		$("div").text = depth;
+		var depth = window.location.pathname.split('/').length-2;
+		//$("div").text(depth);
 		if(!location.includes('0.0')){
 			depth--;
 		}
+		//$("footer").text(depth);
 		var path = "";
 		for(var i = 0; i<depth; i++){
 			path += '../';
